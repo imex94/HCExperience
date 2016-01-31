2edf7f69de6a09752fe46bf97009e1f9f72ec792
Clarence Ji <clare.dev@clarenceji.net>
2016-01-31 02:23:26 +0000

    First View

---
+++ HCExperience/HCExperience/HCEFirstViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HCEFirstViewController.swift
+//  HCExperience
+//
+//  Created by Clarence Ji on 1/31/16.
+//
+//
+
+import UIKit
+
+class HCEFirstViewController: UIViewController {
+
+    @IBOutlet var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        textView.sizeToFit()
+        textView.layoutIfNeeded()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
